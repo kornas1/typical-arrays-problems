1bb467a0d14d59b5c59c64e88eeb0660b7901c11
kornas1 <79769071+kornas1@users.noreply.github.com>
2021-03-11 21:38:49 +0300

    feat: add file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+
+  if(arr == undefined || arr.length == 0){
+    return 0;
+  }
+  else{
+    let min = arr[0];
+    for(let i=1; i<arr.length; i++){
+      if(arr[i]<min){
+        min = arr[i];
+      }
+    }
+    return min;
+  }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+
+  if(arr == undefined || arr.length == 0){
+    return 0;
+  }
+  else{
+    let max = arr[0];
+    for(let i=1; i<arr.length; i++){
+      if(arr[i]>max){
+        max = arr[i];
+      }
+    }
+    return max;
+  }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg (arr) {
+
+  if(arr == undefined || arr.length == 0){
+    return 0;
+  }
+  else{
+  let sum = arr[0];
+      for(let i=1; i<arr.length; i++){
+        sum = sum + arr[i];
+      }
+  
+      let avgNum = sum/arr.length;
+      return avgNum;
+  }
+    return 0;
+  }
